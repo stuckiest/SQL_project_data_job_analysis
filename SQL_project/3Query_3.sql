41b278c04c9c8ba987c3bd3ecb9c55ba2ddfb9c4
stuckiest <stuckiest@gmail.com>
2025-07-05 19:26:36 -0600

    More work on query 3

--- SQL_project/3Query_3.sql
+++ SQL_project/3Query_3.sql
@@ -4,8 +4,8 @@ INNER JOIN skills_job_dim
     ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim
     ON skills_job_dim.skill_id = skills_dim.skill_id
-    --WHERE job_postings_fact.job_title_short = 'Data Analyst'
-    LIMIT 5;
+    WHERE job_postings_fact.job_title_short = 'Data Analyst'
+    LIMIT 50;
 
 /*WITH skill_mentions AS (
     SELECT
