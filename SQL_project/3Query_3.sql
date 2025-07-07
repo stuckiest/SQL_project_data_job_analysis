e73b8037c489572a3026ae0883e473e88d2c5699
stuckiest <stuckiest@gmail.com>
2025-07-07 14:29:59 -0600

    query 3 finished

--- SQL_project/3Query_3.sql
+++ SQL_project/3Query_3.sql
@@ -1,11 +1,16 @@
-SELECT *
+SELECT
+--skills_dim.skill_id,
+COUNT (skills) AS skill_counts,
+skills
 FROM job_postings_fact
 INNER JOIN skills_job_dim 
     ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim
     ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE job_postings_fact.job_title_short = 'Data Analyst'
-    LIMIT 50;
+    GROUP BY skills
+    ORDER BY skill_counts DESC;
+    
 
 /*WITH skill_mentions AS (
     SELECT
